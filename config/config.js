9aaee5292a06a4222bbaa6a7c7cb5da21b735c16
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-17 08:53:01 -0400

    Add .env.example

--- config/config.js
+++ config/config.js
@@ -1,12 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const config = {
-    env: process.env.NODE_ENV || 'development', 
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI ||"mongodb+srv://Blessji:YnyCGmxrHFVGCBeI@cluster0.g4uvc4e.mongodb.net/skeleton?retryWrites=true&w=majority"||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' + 
-   (process.env.MONGO_PORT || '27017') +
-    '/mernproject' 
-    }
-    export default config
-   
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri:
+    process.env.MONGODB_URI || "your_default_mongo_uri_if_needed"
+};
+export default config;
