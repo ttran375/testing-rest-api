4b2f31165e0a7655531d44154fffd738edd55aa1
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-17 13:07:57 +0000

    chore: Update MongoDB URI in config file

--- config/config.js
+++ config/config.js
@@ -5,7 +5,6 @@ const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri:
-    process.env.MONGODB_URI || "your_default_mongo_uri_if_needed"
+  mongoUri: process.env.MONGODB_URI || "your_default_mongo_uri_if_needed",
 };
 export default config;
